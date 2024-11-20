fb07645341f6f139e27c46fc17a9730464f71000
jose1995N <jmatmor313@g.educaand.es>
2024-11-20 14:09:16 +0100

    Add files via upload

--- ejercicio10.java
+++ ejercicio10.java
@@ -2,34 +2,55 @@
 comprendidos entre 0 y 400 (ambos incluidos). A continuación el programa mostrará el array y
  preguntará si el usuario quiere resaltar los múltiplos de 5 o los múltiplos de 7. Seguidamente se
 volverá a mostrar el array escribiendo los números que se quieren resaltar entre corchetes*/
-import java.util.Random;
-import java.util.Scanner;
 
+import java.util.Scanner;
 public class ejercicio10 {
         public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
             int[] array = new int[20];
-            Random rand = new Random();
     
+            
+            System.out.println("Por favor, introduce 20 números enteros entre 0 y 400:");
             for (int i = 0; i < array.length; i++) {
-                array[i] = rand.nextInt(401);
+                System.out.print("Número " + (i + 1) + ": ");
+                int input = scanner.nextInt();
+                
+                if (input >= 0 && input <= 400) {
+                    array[i] = input;
+                } else {
+                    System.out.println("Número fuera del rango (0-400), se establecerá a 0.");
+                    array[i] = 0;
+                }
             }
     
+           
+            System.out.println("Array original:");
             for (int num : array) {
                 System.out.print(num + " ");
             }
             System.out.println();
-            Scanner sc = new Scanner(System.in);
-            System.out.print("¿Múltiplos de 5 o 7? (escribe 5 o 7): ");
-            String op = sc.nextLine();
     
-            for (int num : array) {
-                if ((op.equals("5") && num % 5 == 0) || (op.equals("7") && num % 7 == 0)) {
-                    System.out.print("[" + num + "] ");
-                } else {
-                    System.out.print(num + " ");
+           
+            System.out.print("¿Quieres resaltar los múltiplos de 5 o de 7? (escribe 5 o 7): ");
+            int opcion = scanner.nextInt();
+    
+           
+            if (opcion == 5 || opcion == 7) {
+                for (int i = 0; i < array.length; i++) {
+                    if (array[i] % opcion == 0) {
+                        System.out.print("[" + array[i] + "] ");
+                    } else {
+                        System.out.print(array[i] + " ");
+                    }
                 }
+                System.out.println();
+            } else {
+                System.out.println("Opción no válida");
             }
+    
+            scanner.close();
         }
     }
+    
      
 
