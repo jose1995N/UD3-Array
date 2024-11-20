10e79bd67bc433361a4e46e80a7b8bc3e4a8591d
jose1995N <jmatmor313@g.educaand.es>
2024-11-20 13:51:28 +0100

    Add files via upload

--- ejercicio11.java
+++ ejercicio11.java
@@ -5,20 +5,20 @@ de tabla como se muestra en la figura.*/
 public class ejercicio11 {
         public static void main(String[] args) {
            
-            int[][] numeros = {
-                {1, 2, 3, 4, 5, 6}, 
-                {7, 8, 9, 10, 11, 12}, 
-                {13, 14, 15, 16, 17, 18} 
+            int[][] num = {{0,30,2,0,5}, {75,0,0,0,0}, {13, 14, 15, 16, 17, 18} 
             };
-  
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 6; j++) {
-                    System.out.print(numeros[i][j] + "\t"); 
+            
+            for (int i = 0; i < num.length; i++) {
+                    System.out.print("fila"+ i + " ");
+                for (int index = 0; index < num[i].length; index++) {
+                    System.out.print(num[i][index]+ " ");
                 }
                 System.out.println(); 
+                }
+               
             }
         }
-    }
+    
     
             
     
